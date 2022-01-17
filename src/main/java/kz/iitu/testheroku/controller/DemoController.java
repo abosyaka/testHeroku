3457ad979e8f9fe43adbd7bb1adae8d6a25342e1
Abylaikhan Baidilbekov <a.baidilbekov@infin-it.kz>
2022-01-17 11:51:19 +0600

    added demo controller

---
+++ src/main/java/kz/iitu/testheroku/controller/DemoController.java
@@ -0,0 +1,15 @@
+package kz.iitu.testheroku.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping
+    public String demo(){
+        return "test is ok!";
+    }
+}
