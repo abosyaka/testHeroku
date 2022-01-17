06c1a53ff8693a6a5d4d77bd418895b229de1ccd
Abylaikhan Baidilbekov <a.baidilbekov@infin-it.kz>
2022-01-17 11:08:40 +0600

    first commit

---
+++ src/test/java/kz/iitu/testheroku/TestHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.testheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHerokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
